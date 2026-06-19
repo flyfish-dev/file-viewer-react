36752595f65ca0f2d5277102b4284db79b12cea2
小爬的老粉丝 <727842003@qq.com>
2026-06-19 18:42:45 +0800

    chore: initial public wrapper release

---
+++ src/index.tsx
@@ -0,0 +1,135 @@
+import {
+  forwardRef,
+  useEffect,
+  useImperativeHandle,
+  useMemo,
+  useRef,
+  type CSSProperties,
+  type HTMLAttributes,
+  type MutableRefObject
+} from 'react'
+import {
+  createViewerControllerHandle,
+  mountViewer,
+  type ViewerController,
+  type ViewerControllerHandle,
+  type ViewerMountOptions,
+  type ViewerCoreOptions
+} from './controller.js'
+import { fileViewerCoreRendererRegistry } from '@file-viewer/core'
+
+export type {
+  FileRef,
+  ViewerAiOptions,
+  ViewerArchiveOptions,
+  ViewerCadOptions,
+  ViewerController,
+  ViewerControllerAccessor,
+  ViewerControllerHandle,
+  ViewerDocxOptions,
+  ViewerEvent,
+  ViewerEventHandler,
+  ViewerEventType,
+  ViewerFetchFile,
+  ViewerFetchInput,
+  ViewerMountOptions,
+  ViewerOptions,
+  ViewerPdfOptions,
+  ViewerSpreadsheetOptions,
+  ViewerSearchOptions,
+  ViewerSourceInput,
+  ViewerThemeMode,
+  ViewerToolbarOptions,
+  ViewerToolbarPosition,
+  ViewerTypstOptions,
+  ViewerWatermarkOptions
+} from './controller.js'
+
+export interface FileViewerHandle extends ViewerControllerHandle {}
+
+export interface FileViewerProps
+  extends Omit<HTMLAttributes<HTMLDivElement>, 'children'>,
+    ViewerMountOptions {}
+
+const defaultStyle: CSSProperties = {
+  width: '100%',
+  height: '100%',
+  minHeight: 0
+}
+
+const viewerCoreOptions: ViewerCoreOptions = {
+  registry: fileViewerCoreRendererRegistry
+}
+
+const destroyController = (
+  controllerRef: MutableRefObject<ViewerController | null>,
+  container: HTMLDivElement | null
+) => {
+  controllerRef.current?.destroy()
+  controllerRef.current = null
+  if (container) {
+    container.innerHTML = ''
+  }
+}
+
+export const FileViewer = forwardRef<FileViewerHandle, FileViewerProps>((props, forwardedRef) => {
+  const {
+    url,
+    file,
+    buffer,
+    name,
+    filename,
+    type,
+    size,
+    options,
+    onEvent,
+    style,
+    ...containerProps
+  } = props
+
+  const containerRef = useRef<HTMLDivElement | null>(null)
+  const controllerRef = useRef<ViewerController | null>(null)
+
+  const viewerOptions = useMemo<ViewerMountOptions>(() => ({
+    url,
+    file,
+    buffer,
+    name,
+    filename,
+    type,
+    size,
+    options,
+    onEvent
+  }), [url, file, buffer, name, filename, type, size, options, onEvent])
+
+  useEffect(() => {
+    const container = containerRef.current
+    if (!container || controllerRef.current) {
+      return undefined
+    }
+
+    controllerRef.current = mountViewer(container, viewerOptions, viewerCoreOptions)
+    return () => destroyController(controllerRef, container)
+  }, [])
+
+  useEffect(() => {
+    void controllerRef.current?.update(viewerOptions)
+  }, [viewerOptions])
+
+  useImperativeHandle(forwardedRef, () => createViewerControllerHandle(
+    () => controllerRef.current,
+    () => destroyController(controllerRef, containerRef.current)
+  ), [])
+
+  return (
+    <div
+      {...containerProps}
+      ref={containerRef}
+      style={{ ...defaultStyle, ...style }}
+    />
+  )
+})
+
+FileViewer.displayName = 'FileViewer'
+
+export default FileViewer
